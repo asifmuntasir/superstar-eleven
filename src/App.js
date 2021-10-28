de0638f6914c721aaa0daeb57f2059dee46b3552
asif-96 <shuyeb.dk@gmail.com>
2021-10-28 20:54:17 +0600

    working on files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react/cjs/react.development';
+import fakedata from './component/Fake Data/players_data.json'
+
+function App() {
+
+  const [data, setData] = useState({});
+
+  useEffect(()=> {
+    console.log(fakedata)
+  }, [])
+
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <a
+        href="https://wa.me/01787118444"
+        class="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="ri-whatsapp-line whatsapp-icon"></i>
+      </a>
+    </div>
+  );
+}
+
+export default App;
