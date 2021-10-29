33139da400f6ace2290218bff94a04119807a575
asif-96 <shuyeb.dk@gmail.com>
2021-10-29 15:44:07 +0600

    changes on AppJs file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-import { useEffect} from 'react/cjs/react.development';
+// import { useEffect} from 'react/cjs/react.development';
 import fakedata from './component/Fake Data/players_data.json'
 
 function App() {
 
   // const [data, setData] = useState({});
 
-  useEffect(()=> {
-    console.log(fakedata)
-  }, [])
+  // useEffect(()=> {
+  //   console.log(fakedata)
+  // }, [])
 
 
   return (
     <div className="App">
       <h1>Hello</h1>
+      
       <a
         href="https://wa.me/01787118444"
         class="whatsapp_float"
